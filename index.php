4af959a5c0069834a6da8636f69a29297a47c35c
Seema <seemadhakal52@gmail.com>
2024-07-16 11:01:24 +0545

    initial

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>webspage</title>
+</head>
+<body>
+
+<div class="header">
+<h4>
+seema Dhakal
+</h4>
+<div class="srh">
+<input type="search" placeholder="search" name="search" class="apple">
+<button type="submit" class="apple" >search</button>
+</div>
+</div>
+<div class="img">
+ <img src="girl.jpg" alt="loading">
+</div>
+<div>
+ <h1>Heading 1</h1>
+ <h2>Heading 2</h2>
+ <p>This is a paragraph.Here is some<strong>bold text</strong>,<i>itali text</i>,and some
+ <u>undreline text.</u></p>
+</div>
+<form action="https://www.google.com/">
+ <label for="name">Name:</label><br>
+ <input type="text"name id="name" class="box">
+ <div>
+     <br><label for="gender">Gender</label>
+     <br><input type="radio" name="gender">Male
+     <input type="radio" name="gender">Female
+
+ </div>
+ <br> <input type="checkbox">subscribe to newsletter 
+ <div>
+     <br><label for="country">country:</label>
+     <option value="np">Nepal</option>
+     <option value="usa">USA</option>
+     <option value="in">India</option>
+     <option value="th">others</option>
+ </select>
+ </div>
+ <br> <input type="submit" value="submit" class="submit">
+ </form>
+
+ 
+ 
+
+
+
+
+</body>
+</html>
+</body>
+</html>
